d477f751567a8bff1990143153f02a07f3be83cc
FINCTIVE <finctive@qq.com>
2022-07-21 14:33:48 +0800

    Update setup_go.sh

--- debian/setup_go.sh
+++ debian/setup_go.sh
@@ -10,8 +10,6 @@ go_bin_filename="go1.18.linux-amd64.tar.gz"
 wget https://golang.org/dl/${go_bin_filename}
 sudo tar -zxvf ${go_bin_filename} -C /usr/local/ >> /dev/null
 rm ${go_bin_filename}
-# todo: a better place to set $PATH
-# bash
-echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee /etc/profile.d/go.sh
-# zsh
-echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee ~/.zprofile
+
+# bash & zsh
+echo -e "# go1.18 \nexport PATH=/usr/local/go/bin:\${PATH}" | tee -a ~/.zprofile | tee -a ~/.bashrc
