499dce900eaaf7c9fdcd158af6f384d43e66f70b
FINCTIVE <finctive@qq.com>
2022-10-13 14:20:18 +0800

    feat: add arm support for k8s tools setup

--- debian/setup_k8s_tools.sh
+++ debian/setup_k8s_tools.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 echo "install k8s-cli-tools"
-# kubectl
 sudo apt-get update && sudo apt-get install -y curl git
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
+
+# kubectl
+sudo apt-get install -y ca-certificates
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 # kubectx
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
@@ -12,22 +16,33 @@ sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 # helm
-wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
-tar xvf helm-v3.4.1-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin
-rm helm-v3.4.1-linux-amd64.tar.gz
-rm -rf linux-amd64
-
-# stern
-wget https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64
-chmod +x stern_linux_amd64
-sudo mv stern_linux_amd64 /usr/local/bin/stern
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm ./get_helm.sh
 
 # popeye
-wget https://github.com/derailed/popeye/releases/download/v0.10.0/popeye_Linux_x86_64.tar.gz  
+ARCH=$(uname -m)
+case $ARCH in
+  aarch64) ARCH="arm64";;
+  x86_64) ARCH="x86_64";;
+esac
+wget https://github.com/derailed/popeye/releases/download/v0.10.0/popeye_Linux_${ARCH}.tar.gz  
 mkdir popeye
-tar xf popeye_Linux_x86_64.tar.gz --directory=popeye
-rm popeye_Linux_x86_64.tar.gz  
-chmod +x popeye/popeye
+tar xf popeye_Linux_${ARCH}.tar.gz --directory=popeye
+rm popeye_Linux_${ARCH}.tar.gz
 sudo mv popeye/popeye /usr/local/bin/popeye
 rm -r popeye
+
+# stern
+ARCH=$(uname -m)
+case $ARCH in
+  aarch64) ARCH="arm64";;
+  x86_64) ARCH="x86_64";;
+esac
+wget https://github.com/stern/stern/releases/download/v1.22.0/stern_1.22.0_linux_${ARCH}.tar.gz
+mkdir stern
+tar xf stern_1.22.0_linux_${ARCH}.tar.gz --directory=stern
+rm stern_1.22.0_linux_${ARCH}.tar.gz
+sudo mv stern/stern /usr/local/bin/stern
+rm -r stern
