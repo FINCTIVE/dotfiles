c8c57031436ed3c367c378ce1c2ec8724e9ba03c
FINCTIVE <finctive@qq.com>
2022-12-14 01:36:40 +0800

    chore: mac brew list

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,26 +1,6 @@
 # install brew
-# setup env variables
-
 
 # install
-brew install \ 
-  watch tmux htop ncdu nload jq \
-  chezmoi fzf autojump ranger the_silver_searcher \
-  kubectl kubie helm stern derailed/popeye/popeye \
-  istioctl kind \
-  tldr
+brew install watch tmux htop ncdu nload jq chezmoi fzf autojump ranger the_silver_searcher tldr
 
-# oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-# init dotfiles
-chezmoi init git@github.com:FINCTIVE/dotfiles.git
-chezmoi init https://github.com/FINCTIVE/dotfiles.git
-chezmoi apply
-
-echo "chezmoi diff => diff"
-echo "chezmoi cd => go to the dotfiles folder"
-echo "chezmoi apply => dotfiles folder to computer settings"
-echo "chezmoi re-add => computer settings to dotfiles folder"
-echo "note: push dotfiles folder manually"
+brew install kubectl kubie helm stern derailed/popeye/popeye istioctl kind 
