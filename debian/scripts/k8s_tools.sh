bcf204b731a5938155799cef4dc2375bc45e5449
FINCTIVE <finctive@qq.com>
2023-07-25 19:13:17 +0800

    kubectl install

--- debian/scripts/k8s_tools.sh
+++ debian/scripts/k8s_tools.sh
@@ -4,9 +4,9 @@ echo "install k8s ops tools"
 sudo apt-get update && sudo apt-get install -y curl git
 
 # kubectl
-sudo apt-get install -y ca-certificates
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y ca-certificates gpg apt-transport-https
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
 
