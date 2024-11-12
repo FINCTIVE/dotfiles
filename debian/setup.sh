85e3d7ef3dacccb61bb156e95d0ba9493e12e657
FINCTIVE <finctive@qq.com>
2024-11-12 17:54:55 +0800

    set up shell

--- debian/setup.sh
+++ debian/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "install cli tools"
+echo "Starting to set up CLI tools... 🔭"
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
   procps lsof \
   iproute2 net-tools iputils-ping netcat-openbsd \
   ca-certificates curl wget \
-  htop ncdu nload tcpdump termshark \
+  htop ncdu nload tcpdump \
   git zsh vim \
   zoxide nnn fzf jq
 
@@ -19,3 +19,5 @@ curl -sSL https://starship.rs/install.sh | sh -s -- --yes
 # dotfiles
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply FINCTIVE
 rm ./bin/chezmoi && find bin -empty -type d -delete
+
+echo "Finish setting up CLI tools. 🫡"
